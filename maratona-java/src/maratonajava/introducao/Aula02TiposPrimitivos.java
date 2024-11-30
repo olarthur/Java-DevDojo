ff2a46030d2aa3a3f885ff95ada09aadc42d94cd
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-11-30 20:06:00 -0300

    Create Aula02TiposPrimitivos.java

---
+++ maratona-java/src/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        System.out.println("A idade é: " + age + " anos.");
+    }
+}
