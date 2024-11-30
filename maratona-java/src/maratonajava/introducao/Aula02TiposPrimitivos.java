24ce3bb3be718f1683771c47c04e863a308570fd
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-11-30 20:22:11 -0300

    Update Aula02TiposPrimitivos.java

--- maratona-java/src/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ maratona-java/src/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,17 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         //int, double, float, char, byte, short, long, boolean
         int age = 10;
+        long number = 100000;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = 20;
+        short ageShort = 21;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A';
+
+
         System.out.println("A idade é: " + age + " anos.");
+        System.out.println(verdadeiro);
     }
 }
