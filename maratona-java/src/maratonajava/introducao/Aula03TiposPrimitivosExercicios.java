194d4d6ee6dd82efb498a4f70af49dea269fa711
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-13 18:51:27 -0300

    Update Aula03TiposPrimitivosExercicios.java

--- maratona-java/src/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
+++ maratona-java/src/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
@@ -1,5 +1,15 @@
 package maratonajava.introducao;
 
+import java.util.Date;
+
 public class Aula03TiposPrimitivosExercicios {
-    
+    public static void main(String[] args) {
+
+        String nome = "Arthur";
+        String endereco = "Rua Um dois - 1";
+        double salario = 2000;
+        Date data = new Date();
+        System.out.println("Eu " + nome + ", morando no endereço " + endereco + ", confirmo que recebi o salário de " + salario + ", na data " + data.toString() + ".");
+        
+    }
 }
