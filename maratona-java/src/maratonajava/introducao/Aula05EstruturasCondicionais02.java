f8e729dd048660793f982e7c9993838e4003df83
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-14 18:58:35 -0300

    Update Aula05EstruturasCondicionais02.java

--- maratona-java/src/maratonajava/introducao/Aula05EstruturasCondicionais02.java
+++ maratona-java/src/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -2,6 +2,17 @@ package maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais02 {
     public static void main(String[] args) {
-        
+        int idade = 17;
+        String categoria;
+
+        if (idade < 15) {
+            categoria = "Categoria Infantil.";
+        } else if (idade >= 15 && idade < 18) {
+            categoria = "Categoria Juvenil.";
+        } else {
+            categoria = "Categoria Adulto.";
+        }
+
+        System.out.println(categoria);
     }
 }
