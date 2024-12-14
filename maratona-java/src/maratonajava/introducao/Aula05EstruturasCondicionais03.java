e63fcfb02d0253f0390e86749a6ae3d110d1ddca
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-14 19:13:24 -0300

    Create Aula05EstruturasCondicionais03.java

---
+++ maratona-java/src/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,16 @@
+package maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        // Operador Ternário   variavel = (condicao) ? valorSeVerdadeiro : valorSeFalso;
+
+        double salario = 6000;
+        String msnDoar = "Doar 5 reais";
+        String msnNaoDoar = "Não doar";
+
+        String resultado = salario > 5000 ? msnDoar : msnNaoDoar;
+
+        System.out.println(resultado);
+        
+    }
+}
