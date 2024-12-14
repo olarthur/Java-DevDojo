48e199799c3a22f195098aab047b7af7382df226
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-14 16:55:36 -0300

    Update Aula04Operadores.java

--- maratona-java/src/maratonajava/introducao/Aula04Operadores.java
+++ maratona-java/src/maratonajava/introducao/Aula04Operadores.java
@@ -1,5 +1,19 @@
 package maratonajava.introducao;
 
 public class Aula04Operadores {
-    
+    public static void main(String[] args) {
+        // Os operdaores aritiméticos são '+' '-' '/' '*'.
+
+        double number01 = 10;
+        double number02 = 20;
+        double soma = number01 + number02;
+        double sub = number02 - number01;
+        double multi = number01 * number02;
+        double div = number02 / number01;
+
+        System.out.println(soma);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(div);
+    }
 }
