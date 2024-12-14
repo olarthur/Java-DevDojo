563e1a716c2963e0d4af03ab93aa43942b78185c
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-14 18:36:23 -0300

    Update Aula05EstruturasCondicionais.java

--- maratona-java/src/maratonajava/introducao/Aula05EstruturasCondicionais.java
+++ maratona-java/src/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -2,6 +2,11 @@ package maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais {
     public static void main(String[] args) {
-        
+        int idade = 20;
+    
+        if (idade >= 18) {
+            System.out.println("É maior de idade.");
+        }
+
     }
 }
