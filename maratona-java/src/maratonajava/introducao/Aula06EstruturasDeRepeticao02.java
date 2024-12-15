a1cf67db0e6c8064a7fe2ee2e2b00d4139bd1a1b
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-15 16:46:56 -0300

    Create Aula06EstruturasDeRepeticao02.java

---
+++ maratona-java/src/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,13 @@
+package maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 100; i++) {
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+        
+    }
+}
