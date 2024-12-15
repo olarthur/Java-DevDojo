a0e307e1e163b005f27610c6389d95878b2b8ce0
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-15 16:25:06 -0300

    Create Aula06EstruturasDeRepeticao01.java

---
+++ maratona-java/src/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
@@ -0,0 +1,28 @@
+package maratonajava.introducao;
+
+import java.util.Arrays;
+
+public class Aula06EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        // 'while' , 'do while' , 'for'
+        int count = 0;
+
+        while(count < 11) {
+            System.out.println(count);
+            count++;
+        }
+
+        System.out.println();
+
+        do {
+            System.out.println(++count);
+        } while(count < 20);
+
+        System.out.println();
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println("For " + i);
+        }
+
+    }
+}
