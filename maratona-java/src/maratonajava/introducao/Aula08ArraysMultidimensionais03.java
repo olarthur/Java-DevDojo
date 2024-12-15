9ac0356ad356de4db387e6eee0ae52ed5c8f0b5d
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-15 19:14:31 -0300

    Create Aula08ArraysMultidimensionais03.java

---
+++ maratona-java/src/maratonajava/introducao/Aula08ArraysMultidimensionais03.java
@@ -0,0 +1,16 @@
+package maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais03 {
+    public static void main(String[] args) {
+
+        int[][] arrayInt = {{0,0}, {1,2,2,3,5,6,7}, {0,0,0,0}, {1,6,8,90,234,324}};
+
+        for (int[] arrayBase : arrayInt){
+            System.out.println("\n--------");
+            for (int num : arrayBase) {
+                System.out.print(num + " ");
+            }
+        }
+
+    }
+}
