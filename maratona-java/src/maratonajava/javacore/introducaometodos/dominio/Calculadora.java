2a812c27c44be7c03eda90dc164cb3cc093884b0
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-16 17:18:00 -0300

    calc2

--- maratona-java/src/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
+++ maratona-java/src/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
@@ -9,4 +9,8 @@ public class Calculadora {
     public void subtraiDoisNumeros(){
         System.out.println(10 - 10);
     }
+
+    public void multiplicaDoisNumeros(int num1, int num2){
+        System.out.println("A multiplicação dos dois números é: " + num1 * num2);
+    }
 }
---
+++ maratona-java/src/maratonajava/javacore/introducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,11 @@
+package maratonajava.javacore.introducaometodos.test;
+
+import maratonajava.javacore.introducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(23, 32);
+    }
+}
