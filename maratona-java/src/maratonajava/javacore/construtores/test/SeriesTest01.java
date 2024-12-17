ac7b4ce3fab55d4f652e6bdbd497c536c5c2ff8c
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-17 18:07:29 -0300

    Update SeriesTest01.java

--- maratona-java/src/maratonajava/javacore/construtores/test/SeriesTest01.java
+++ maratona-java/src/maratonajava/javacore/construtores/test/SeriesTest01.java
@@ -4,10 +4,8 @@ import maratonajava.javacore.construtores.dominio.Series;
 
 public class SeriesTest01 {
     public static void main(String[] args){
-        Series series = new Series("Sopranos", "Crime", 13, "Ação");
-        Series series2 = new Series();
+        Series series = new Series("Sopranos", "Crime", 13, "Ação", "HBO");
 
-        series2.imprime();
         series.imprime();
     }
 }
