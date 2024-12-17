1e3621c7a692d7450360090e757684d9459f3157
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-17 19:38:20 -0300

    Create SeriesTest01.java

---
+++ maratona-java/src/maratonajava/javacore/modificadorestatico/test/SeriesTest01.java
@@ -0,0 +1,11 @@
+package maratonajava.javacore.modificadorestatico.test;
+
+import maratonajava.javacore.modificadorestatico.dominio.Series;
+
+public class SeriesTest01 {
+    public static void main(String[] args){
+        Series serie = new Series();
+        Series series1 = new Series();
+        Series series2 = new Series();
+    }
+}
