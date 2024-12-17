54e1c329a33bd7783817f5c7da22828b66041bd1
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-17 17:00:12 -0300

    Update Funcionario.java

--- maratona-java/src/maratonajava/javacore/introducaometodos/dominio/Funcionario.java
+++ maratona-java/src/maratonajava/javacore/introducaometodos/dominio/Funcionario.java
@@ -2,9 +2,10 @@ package maratonajava.javacore.introducaometodos.dominio;
 
 public class Funcionario {
 
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
 
     public void imprimirDados() {
         System.out.println("Nome: " + this.nome);
@@ -26,8 +27,6 @@ public class Funcionario {
             return;
         }
 
-        double media = 0;
-
         for (double salario : salarios) {
             media += salario;
         }
@@ -35,4 +34,33 @@ public class Funcionario {
 
         System.out.println("\nMédia Salárial: " + media);
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+    
 }
