daf99f9b0ade8871e703297c91b04b80b6f6174c
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-18 18:29:50 -0300

    Create JogadorTest02.java

---
+++ maratona-java/src/maratonajava/javacore/associacao/test/JogadorTest02.java
@@ -0,0 +1,15 @@
+package maratonajava.javacore.associacao.test;
+
+import maratonajava.javacore.associacao.dominio.Jogador;
+import maratonajava.javacore.associacao.dominio.Time;
+
+public class JogadorTest02 {
+    public static void main(String[] args){
+        Jogador jogador1 = new Jogador("Pelé");
+        Time time = new Time("Seleção Brasileira");
+
+        jogador1.setTime(time);
+
+        jogador1.imprime();
+    }
+}
