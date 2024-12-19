72ac46c7954b5c13d5f135580682d01e7efedd82
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-19 19:38:28 -0300

    Create Funcionario.java

---
+++ maratona-java/src/maratonajava/javacore/heranca/dominio/Funcionario.java
@@ -0,0 +1,13 @@
+package maratonajava.javacore.heranca.dominio;
+
+public class Funcionario extends Pessoa {
+    private double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
