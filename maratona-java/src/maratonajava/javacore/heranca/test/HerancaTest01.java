3ddb1795bbbd3547696101295baf69d317eef31a
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-19 19:38:33 -0300

    Create HerancaTest01.java

---
+++ maratona-java/src/maratonajava/javacore/heranca/test/HerancaTest01.java
@@ -0,0 +1,30 @@
+package maratonajava.javacore.heranca.test;
+
+import maratonajava.javacore.heranca.dominio.Endereco;
+import maratonajava.javacore.heranca.dominio.Funcionario;
+import maratonajava.javacore.heranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua Um do Dois, 3");
+        endereco.setCep("08888-880");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Sêneca");
+        pessoa.setCpf("088.888.888-80");
+        pessoa.setEndereco(endereco);
+
+        pessoa.imprime();
+
+        System.out.println("----------------------");
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Epiteto");
+        funcionario.setCpf("088.888.888-80");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(2000);
+
+        funcionario.imprime();
+    }
+}
