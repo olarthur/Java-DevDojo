9e4edf92cb253f0dde0b623607cfc5b93d3d2698
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-19 18:19:04 -0300

    Create Local.java

---
+++ maratona-java/src/maratonajava/javacore/associacao/exercicio/dominio/Local.java
@@ -0,0 +1,17 @@
+package maratonajava.javacore.associacao.exercicio.dominio;
+
+public class Local {
+    private String endereco;
+
+    public Local(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
