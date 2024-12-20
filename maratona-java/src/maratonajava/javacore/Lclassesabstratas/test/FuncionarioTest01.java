ac103b71416d1a782e273da32c7fe670a5df2f1d
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-20 17:53:54 -0300

    Abstract

---
+++ maratona-java/src/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,15 @@
+package maratonajava.javacore.Lclassesabstratas.test;
+
+import maratonajava.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import maratonajava.javacore.Lclassesabstratas.dominio.Funcionario;
+import maratonajava.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Sêneca", 14000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Marco Aurélio", 8000);
+
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+    }
+}
