9e83d5a81adec101b7ee96c1fc8b15c7d635e698
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-20 15:05:50 -0300

    Create SeriesTest01.java

---
+++ maratona-java/src/maratonajava/javacore/sobrescrita/test/SeriesTest01.java
@@ -0,0 +1,10 @@
+package maratonajava.javacore.sobrescrita.test;
+
+import maratonajava.javacore.sobrescrita.dominio.Series;
+
+public class SeriesTest01 {
+    public static void main(String[] args) {
+        Series series = new Series("Succession");
+        System.out.println(series);
+    }
+}
