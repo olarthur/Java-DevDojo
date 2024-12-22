c4555410b37e5dea8d4ed00baccf5cdd8eb5b594
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-22 17:00:59 -0300

    Update ProdutoTest01.java

--- maratona-java/src/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ maratona-java/src/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package maratonajava.javacore.Npolimorfismo.test;
 
 import maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,9 +9,12 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("NUC10i7", 12000);
         Tomate tomate = new Tomate("Cereja", 7.00);
+        Televisao televisao = new Televisao("Sansung 50\" ", 7000);
 
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("-------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("-------------------");
+        CalculadoraImposto.calcularImposto(televisao);
     }
 }
