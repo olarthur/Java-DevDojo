51583adcd5e7037ea842a22a507856f2cddf7c15
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-22 16:22:53 -0300

    Create Tomate.java

---
+++ maratona-java/src/maratonajava/javacore/Npolimorfismo/dominio/Tomate.java
@@ -0,0 +1,14 @@
+package maratonajava.javacore.Npolimorfismo.dominio;
+
+public class Tomate extends Produto{
+    public static final double IMPOSTO_POR_CENTO = 0.06;
+    public Tomate(String nome, double valor) {
+        super(nome, valor);
+    }
+
+    @Override
+    public double calcularImposto() {
+        System.out.println("Calculando Imposto do Tomate!");
+        return this.valor * IMPOSTO_POR_CENTO;
+    }
+}
