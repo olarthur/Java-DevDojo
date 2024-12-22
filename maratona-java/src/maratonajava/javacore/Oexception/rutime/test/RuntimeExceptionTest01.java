399531641e4b1a8efb01e8caa0879d8042fe9351
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-22 18:20:48 -0300

    Create RuntimeExceptionTest01.java

---
+++ maratona-java/src/maratonajava/javacore/Oexception/rutime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,9 @@
+package maratonajava.javacore.Oexception.rutime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        // Checked  Unchecked
+        Object object = null;
+        System.out.println(object.toString());
+    }
+}
