092fdcff9d75248f50738552040ebfaf8d0ac6e8
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-26 19:58:55 -0300

    Resource Bundle

---
+++ maratona-java/src/maratonajava/javacore/Tresourcebundle/test/ResourceBundleTest01.java
@@ -0,0 +1,17 @@
+package maratonajava.javacore.Tresourcebundle.test;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ResourceBundleTest01 {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", new Locale("pt", "BR"));
+        System.out.println(bundle.getString("ola"));
+        System.out.println(bundle.getString("bom.dia"));
+
+        bundle = ResourceBundle.getBundle("messages", new Locale("en", "US"));
+        System.out.println(bundle.getString("ola"));
+        System.out.println(bundle.getString("bom.dia"));
+    }
+}
