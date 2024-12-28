4b263887b423e05c8765b185c55ac5c35a7ca05c
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-12-28 20:28:44 -0300

    SimpleFileVisitor

---
+++ maratona-java/src/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest01.java
@@ -0,0 +1,23 @@
+package maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class ListJavaFiles extends SimpleFileVisitor<Path> {
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        // busca todos os arquivos '.java' do diretório
+        if (file.getFileName().toString().endsWith(".java")) {
+            System.out.println(file.getFileName());
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+public class SimpleFileVisitorTest01 {
+    public static void main(String[] args) throws IOException{
+        Path root = Paths.get(".");
+        Files.walkFileTree(root, new ListJavaFiles());
+    }
+}
