29eead3c3675fa82380472dc4d50c166877cec00
Arthur Oliveira <arthur08oliveira01@gmail.com>
2025-01-05 17:22:35 -0300

    Finding and Matching

--- maratona-java/src/maratonajava/javacore/ZZEstreams/test/StreamTest06.java
+++ maratona-java/src/maratonajava/javacore/ZZEstreams/test/StreamTest06.java
@@ -1,7 +1,42 @@
 package maratonajava.javacore.ZZEstreams.test;
 
+import maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class StreamTest06 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("The Beginning", 8.99),
+            new LightNovel("The Lord of the Rings", 3.99),
+            new LightNovel("Vagabond", 5.99),
+            new LightNovel("O Alienista", 2.99),
+            new LightNovel("Dom Casmurro", 5.99),
+            new LightNovel("Hamlet", 1.99),
+            new LightNovel("Hamlet", 1.99),
+            new LightNovel("Otelo", 4.00)
+    ));
+
     public static void main(String[] args) {
-        
+        System.out.println(lightNovels.stream().anyMatch(ln -> ln.getPrice() > 8));
+        System.out.println(lightNovels.stream().allMatch(ln -> ln.getPrice() > 0));
+        System.out.println(lightNovels.stream().noneMatch(ln -> ln.getPrice() > 0));
+
+        lightNovels.stream()
+                .filter(ln -> ln.getPrice() > 3)
+                .findAny()
+                .ifPresent(System.out::println);
+
+        lightNovels.stream()
+                .filter(ln -> ln.getPrice() > 3)
+                .sorted(Comparator.comparing(LightNovel::getPrice).reversed())
+                .findFirst()
+                .ifPresent(System.out::println);
+
+        lightNovels.stream()
+                .filter(ln -> ln.getPrice() > 3)
+                .max(Comparator.comparing(LightNovel::getPrice))
+                .ifPresent(System.out::println);
     }
 }
